a0e9d9ca8e1e08958f23ce2d1385bf0fb3d86656
unknown <ioan-traianluca@labpc254.computing.dundee.ac.uk>
2016-03-14 19:23:30 +0000

    h

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -7,26 +7,29 @@ public class Test extends PApplet {
     private Serial serialPort;
 
     public void initSerialConnection() {
-        try {
-            serialPort = new Serial(this, "COM3", 9600);
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
-
+        serialPort = new Serial(this, "COM6", 9600);
     }
 
     @Override
     public void setup() {
         initSerialConnection();
+
     }
 
     @Override
     public void settings() {
-
+        size(640, 360);
     }
 
     @Override
     public void draw() {
+        /* if (serialPort.available() > 0) {
+         String input = serialPort.readStringUntil('\n');
+         if (input != null) {
+         input = input.trim();
+         System.out.println(input);
+         }
+         }*/
     }
 
     public static void main(String[] args) {
